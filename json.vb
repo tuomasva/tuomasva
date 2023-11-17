f19ff60f9493d3fe87573bf48b6a8ed7337ee89e
Tuomas Valtonen <tuomas.valtonen@mezzoforte.fi>
2023-11-17 07:54:21 +0000

    Update json.vb

--- json.vb
+++ json.vb
@@ -1,25 +1,33 @@
-Data = encoding.GetBytes(postParameter)
-Dim request As Net.HttpWebRequest = Net.WebRequest.Create(postUrl) ' Login location taken from the form action
+Imports System.Text
+Imports System.Net
+Imports System.IO
+
+' Your JSON data
+Dim jsonData As String = "{""username"":""username@example.fi"",""password"":""password""}"
+' Convert JSON data to a byte array
+Dim byteArray As Byte() = Encoding.UTF8.GetBytes(jsonData)
+
+' Create request
+Dim request As Net.HttpWebRequest = Net.WebRequest.Create(postUrl) ' URL
 
 With request
     .KeepAlive = True
-    .UserAgent = "Mozilla"
+    .UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
     .Method = "POST"
     .AllowAutoRedirect = True
-    .ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
-    .UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+    .ContentType = "application/json" ' Changed to application/json
     .Headers.Add("X-Requested-With", "XMLHttpRequest")
-    If csrfToken <> "" Then
+    If Not String.IsNullOrEmpty(csrfToken) Then
         .Headers.Add("X-Csrf-Token", csrfToken)
     End If
-    If xsrfToken <> "" Then
+    If Not String.IsNullOrEmpty(xsrfToken) Then
         .Headers.Add("X-xsrf-Token", xsrfToken)
     End If
-    .ContentLength = Data.Length
+    .ContentLength = byteArray.Length
     .CookieContainer = cookies
 End With
 
-Dim SendReq As IO.Stream = request.GetRequestStream
-SendReq.Write(Data, 0, Data.Length)
-SendReq.Close()
-
+' Write data to request stream
+Using sendReq As IO.Stream = request.GetRequestStream()
+    sendReq.Write(byteArray, 0, byteArray.Length)
+End Using
